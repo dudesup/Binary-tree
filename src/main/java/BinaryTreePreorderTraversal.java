791cc7d94d05048b1c2c840970deee41eb52807e
Daria Supińska <dariasupinska@gmail.com>
2022-11-27 23:50:12 +0100

    solution added

--- src/main/java/BinaryTreePreorderTraversal.java
+++ src/main/java/BinaryTreePreorderTraversal.java
@@ -1,7 +1,23 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class BinaryTreePreorderTraversal {
     public List<Integer> preorderTraversal(TreeNode root) {
-        return List.of(1, 2, 3);
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) return output;
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pollLast();
+            output.add(node.val);
+            if (node.right != null) {
+                stack.add(node.right);
+            }
+            if (node.left != null) {
+                stack.add(node.left);
+            }
+        }
+        return output;
     }
 }
